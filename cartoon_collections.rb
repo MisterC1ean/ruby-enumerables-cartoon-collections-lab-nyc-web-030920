c6461afe9538e425dc1d1a7d7fada17dc9e9a189
Robert Keller <robert_keller44@icloud.com>
2020-01-19 20:55:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,14 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any do |x|
+    if x.length < 4
+      false
+    else
+      true
+    end
+  end
 end
 
 def find_the_cheese# code an argument here
