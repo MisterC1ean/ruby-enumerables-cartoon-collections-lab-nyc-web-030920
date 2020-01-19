86e09532c6d0feb9daec5e8caeb3ba4c50afbd09
Robert Keller <robert_keller44@icloud.com>
2020-01-19 21:01:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,12 +11,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any do |x|
-    if x.length < 4
-      false
-    else
-      true
-    end
+  calls.any? do |x|
+   x.length > 4
   end
 end
 
