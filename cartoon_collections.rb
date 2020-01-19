0cf24b0477e699f59f398c9913b09777ebe02a1c
Robert Keller <robert_keller44@icloud.com>
2020-01-19 21:33:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,9 @@ def find_the_cheese(ingredients)
  cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
   if ingredients.include? (cheese)
-    cheese
+    return cheese
+  else
+    return nil
   end
   end
 end
