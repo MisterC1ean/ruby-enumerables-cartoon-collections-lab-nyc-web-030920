0ee6d7ba622335fa3b8ab7a446e3d66bc245dd2e
Robert Keller <robert_keller44@icloud.com>
2020-01-19 20:44:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_array)
+dwarf_array.each_with_index do |x, index|
+  puts "/#{index + 1}. *#{x}/"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |x|
+    planeteer_calls = "#{x}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
